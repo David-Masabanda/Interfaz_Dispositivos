013007f8a97ba06ce729a9e2b656b648b7a358cd
jessicalyruiz <jessicalyr@gmail.com>
2023-07-25 19:45:46 -0500

    Gps y Camara

---
+++ app/src/main/java/com/example/pruebas/ui/activities/CameraActivity.kt
@@ -0,0 +1,36 @@
+package com.example.pruebas.ui.activities
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.pruebas.databinding.ActivityCameraBinding
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCaptura.setOnClickListener{
+            val i =Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            cameraResult.launch(i)
+        }
+    }
+
+    private val cameraResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result->
+        when(result.resultCode){
+            Activity.RESULT_OK->{
+                val image= result.data?.extras?.get("data") as Bitmap
+                binding.imgPersona.setImageBitmap(image)
+            }
+            Activity.RESULT_CANCELED->{}        }
+    }
+}
