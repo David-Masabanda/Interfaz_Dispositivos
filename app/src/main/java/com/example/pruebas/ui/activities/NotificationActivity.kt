88e7077ce54a80ff5e2b85451b8a5dcb2fc4226d
jessicalyruiz <jessicalyr@gmail.com>
2023-07-31 19:51:37 -0500

    Tiempo

--- app/src/main/java/com/example/pruebas/ui/activities/NotificationActivity.kt
+++ app/src/main/java/com/example/pruebas/ui/activities/NotificationActivity.kt
@@ -1,18 +1,25 @@
 package com.example.pruebas.ui.activities
 
 import android.annotation.SuppressLint
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.pruebas.R
 import com.example.pruebas.databinding.ActivityNotificationBinding
+import com.example.pruebas.ui.utilities.BroadcasterNotifications
+import java.util.Calendar
+import java.util.Date
 
 class NotificationActivity : AppCompatActivity() {
 
@@ -23,14 +30,44 @@ class NotificationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityNotificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        createNotificationChannel()
 
         binding.btnNotificacion.setOnClickListener{
             Log.d("UCE", "Entrando")
 
             //La comento una vez que ya esta creado el canal
-//            createNotificationChannel()
             sendNotification()
         }
+
+        binding.btnNotiprogramada.setOnClickListener{
+            val calendar= Calendar.getInstance()
+            val hora = binding.time.hour
+            val minutes= binding.time.minute
+
+            Toast.makeText(this, "La notificacion se activa a las: $hora con $minutes", Toast.LENGTH_SHORT).show()
+
+            calendar.set(Calendar.HOUR, hora)
+            calendar.set(Calendar.MINUTE, minutes)
+            calendar.set(Calendar.SECOND,0)
+
+            sendNotificationTimePicker(calendar.timeInMillis)
+
+        }
+    }
+
+    private fun sendNotificationTimePicker(time : Long){
+        Log.d("UCE", "entra al metodo")
+        val myIntent= Intent(applicationContext, BroadcasterNotifications::class.java)
+        val myPendingIntent=PendingIntent.getBroadcast(
+            applicationContext,
+            0,
+            myIntent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager= getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP,time, myPendingIntent)
+        Log.d("UCE", "termina")
     }
 
 
@@ -56,8 +93,7 @@ class NotificationActivity : AppCompatActivity() {
 
 
     private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "Variedades"
             val descriptionText = "Notificaciones simples"
@@ -65,7 +101,7 @@ class NotificationActivity : AppCompatActivity() {
             val channel = NotificationChannel(CHANNEL, name, importance).apply {
                 description = descriptionText
             }
-            // Register the channel with the system
+
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
